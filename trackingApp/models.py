a3b05e8a74365bfbdd01df3e17bc2d77556e1dc0
MaxMedvedenko <medvedenkomaksim143@gmail.com>
2024-03-29 13:41:55 +0200

    added models

--- trackingApp/models.py
+++ trackingApp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    STATUS_CHOICES = [
+        ('TODO', 'To Do'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('DONE', 'Done'),
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='TODO')
+    PRIORITY_CHOICES = [
+        ('LOW', 'Low'),
+        ('MEDIUM', 'Medium'),
+        ('HIGH', 'High'),
+    ]
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES, default='LOW')
+    due_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, related_name='comments', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.user.username} on {self.task.title}'
