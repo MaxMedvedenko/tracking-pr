4681291b1ebad6bd6ab6250b98320763139ff231
MaxMedvedenko <medvedenkomaksim143@gmail.com>
2024-04-05 16:40:48 +0300

    registered model in admin.py

--- trackingApp/admin.py
+++ trackingApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Task, Comment, Note, NoteComment
 # Register your models here.
+
+admin.site.register(Task)
+admin.site.register(Comment)
+admin.site.register(Note)
+admin.site.register(NoteComment)
